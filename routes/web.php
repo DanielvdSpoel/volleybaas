809d090fa07c287e41b25a5da31d022f8db58a0c
Daniël van der Spoel <contact@danielvdspoel.nl>
2023-07-11 19:56:54 +0200

    Change randomiser

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return inertia('Home', [
-        'volleybaas' => fake()->boolean() ? 'Kevin' : 'Trevor',
+        'volleybaas' => (bool)rand(0,1) ? 'Kevin' : 'Trevor',
     ]);
 });
